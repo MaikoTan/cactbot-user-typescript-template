9433e3d66e8b391522f3ae79ba756552775b438f
Maiko Sinkyaet Tan <maiko.tan.coding@gmail.com>
2022-09-27 06:37:36 +0000

    fix: remove customizations in radar

--- src/radar/index.ts
+++ src/radar/index.ts
@@ -1,34 +1,22 @@
 declare const Options: typeof RadarOptions;
 
-// You can add your own monster list here.
-Options.CustomMonsters = {
-  Any: {
-    name: {
-      // monster names
-      en: 'ya-te-veo',
-      cn: '食人花',
-      ja: 'ヤテベオ',
-    },
-    hp: 1000, // minimum hp of the monster
-    rank: 'Custom', // rank
-  },
-};
-
-// You can also add different options to different ranks.
-Options.RankOptions = {
-  S: {
-    DetectionRange: 100,
-    TTS: true,
-    Type: 'mob', // can be 'mob' or 'any'
-    Enabled: true,
-  },
-  Custom: {
-    DetectionRange: 500,
-    TTS: false,
-    PopSoundAlert: true,
-    Type: 'any',
-    Enabled: true,
-  },
-};
+// You can make some changes to the default options here.
+// For example, add some custom monsters.
+// Options.CustomMonsters = {
+//   Any: {
+//     name: {
+//       // monster names
+//       en: 'ya-te-veo',
+//       cn: '食人花',
+//       ja: 'ヤテベオ',
+//     },
+//     hp: 1000, // minimum hp of the monster
+//     rank: 'Custom', // rank
+//   },
+// };
+// All types of options are defined in
+// https://github.com/quisquous/cactbot/blob/main/ui/radar/radar_config.ts
+// Or you can check the example config file in
+// https://github.com/quisquous/cactbot/blob/main/user/radar-example.js
 
 export {};
