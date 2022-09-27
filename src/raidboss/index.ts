77bac081c0aaeeddadf40b2365d1ef72c6592127
Maiko Sinkyaet Tan <maiko.tan.coding@gmail.com>
2022-09-27 14:41:54 +0800

    fix: remove customisations (#4)

--- src/raidboss/index.ts
+++ src/raidboss/index.ts
@@ -1,32 +1,18 @@
 declare const Options: typeof RaidbossOptions;
 
-// Path to sound played for info-priority text popups, or when "Info" is
-// specified as the sound name.
-Options.InfoSound = '../../resources/sounds/freesound/percussion_hit.ogg';
-
-// Path to sound played for alert-priority text popups, or when "Alert" is
-// specified as the sound name.
-Options.AlertSound = '../../resources/sounds/BigWigs/Alert.ogg';
-
-// Path to sound played for alarm-priority text popups, or when "Alarm" is
-// specified as the sound name.
-Options.AlarmSound = '../../resources/sounds/BigWigs/Alarm.ogg';
-
-// Path to sound played when "Long" is specified as the sound name.
-Options.LongSound = '../../resources/sounds/BigWigs/Long.ogg';
-
-// Path to sound played when the fight starts, or when "Pull" is
-// specified as the sound name.
-Options.PullSound = '../../resources/sounds/freesound/sonar.ogg';
-
-// A set of nicknames to use for players, when trying to shorten names.
-// See: https://github.com/quisquous/cactbot/blob/main/docs/CactbotCustomization.md#customizing-behavior
-Options.PlayerNicks = {
-  'Captain Jimmy': 'Jimmy',
-  'Pipira Pira': 'Fish',
-};
+// You can make some changes to the default options here.
+// For example, set nicknames for some players.
+// Options.PlayerNicks = {
+//   'Captain Jimmy': 'Jimmy',
+//   'Pipira Pira': 'Fish',
+// };
+// All types of options are defined in
+// https://github.com/quisquous/cactbot/blob/main/ui/raidboss/raidboss_options.ts
+// Or you can check the example config file in
+// https://github.com/quisquous/cactbot/blob/main/user/raidboss-example.js
 
 // Add triggers in `data/` recursively.
+// NOTE: You SHOULD NOT change these lines, or it may cause unexpected errors.
 const context = require.context('./data', true, /.*\.(ts|js|tsx)$/);
 context.keys().forEach((key) => {
   const triggerFile = context(key);
